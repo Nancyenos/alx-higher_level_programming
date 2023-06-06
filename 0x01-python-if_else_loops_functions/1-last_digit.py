2775c6704b1ef61f311934bd137553898923acf7
Nancyenos <nicymimz@gmail.com>
2023-06-06 14:08:20 -0700

    print alphabet without q and e

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ number = random.randint(-10000, 10000)
 n = abs(number) % 10
 if n > 5:
     print("Last digit of", number, "is", n, "and is greater than 5")
-elif number < 0:
+    if number < 0:
     print("Last digit of", number, "is", -1*n, "and is less than 6 and not 0")
 elif n == 0:
     print("Last digit of", number, "is", n, "and is 0")
