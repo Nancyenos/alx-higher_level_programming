206ec52a517864e09fcdd1091b9815244fe17890
Nancyenos <nicymimz@gmail.com>
2023-06-10 11:09:13 -0700

    print number of arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+arg_name = sys.argv[1:]
+num_args = len(sys.argv) - 1
+if num_args == 0:
+    print("{} arguments.".format(num_args))
+elif num_args == 1:
+    print("{} argument:".format(num_args))
+else:
+    print("{} arguments:".format(num_args))
+for i in range(len(arg_name)):
+    print("{}: {}".format(i+1, arg_name[i]))
