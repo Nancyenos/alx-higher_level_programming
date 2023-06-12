6ed775e3cb690976221a91b86098d2ad13d7c330
Nancyenos <nicymimz@gmail.com>
2023-06-11 23:31:31 -0700

    print integers vertically

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for inti in my_list:
-        print("{0}".format(inti))
+    for inti in range(len(my_list)):
+        print("{:d}".format(my_list[inti]))
